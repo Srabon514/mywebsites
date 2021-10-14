3b642fe41257fb5b1d5d6218ab1f147602cfe7a6
Srabon514 <89089499+Srabon514@users.noreply.github.com>
2021-10-14 13:36:04 +0600

    landingpage added

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(".banner-slider").slick({
+    fade:true,
+    autoplay:true,
+    prevArrow:'<i class="fas fa-arrow-circle-left prev"></i>',
+    nextArrow:'<i class="fas fa-arrow-circle-right next"></i>',
+    
+})
+$('.venobox').venobox();
