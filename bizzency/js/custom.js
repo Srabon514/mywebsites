3d734ce37b461cd581ea47d2f2224214b1278330
Srabon514 <89089499+Srabon514@users.noreply.github.com>
2021-10-14 13:22:57 +0600

    2nd added

---
+++ bizzency/js/custom.js
@@ -0,0 +1,17 @@
+$(".search").click(function(){
+    $(".searchbox").fadeIn()
+    
+})
+$(".close").click(function(){
+    $(".searchbox").fadeOut()
+    
+})
+$(".searchbox").click(function(){
+    $(".searchbox").fadeOut()
+    
+})
+$(".bar").click(function(){
+    $(".items").fadeToggle()
+    
+})
+
