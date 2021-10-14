63cd2961163f18d0c01a8defcc692dca9c04a057
Srabon514 <89089499+Srabon514@users.noreply.github.com>
2021-10-14 13:18:45 +0600

    added

---
+++ valentina/js/custom.js
@@ -0,0 +1,8 @@
+$(".slider").slick({
+    slidesToShow: 2,
+    autoplay: true,
+    prevArrow: '<i class="fas fa-chevron-left prev"></i>', 
+    nextArrow: '<i class="fas fa-chevron-right next"></i>', 
+    
+})
+
