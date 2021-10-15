a7c8bd436d09d0048bc9553351a3c7ec83336bf4
Srabon514 <89089499+Srabon514@users.noreply.github.com>
2021-10-15 20:09:59 +0600

    hunt added

---
+++ HUNT/js/custom.js
@@ -0,0 +1,17 @@
+$(".banner-slider").slick({
+    fade:true,
+    autoplay:true,
+    prevArrow:'<i class="fas fa-arrow-circle-left prev"></i>',
+    nextArrow:'<i class="fas fa-arrow-circle-right next"></i>',
+    
+})
+
+$('.venobox').venobox();
+
+$(".service-slider").slick({
+    vertical:true,
+    slidesToShow:3,
+    centerMode: true,
+    prevArrow:'<i class="fas fa-chevron-up up"></i>',
+    nextArrow:'<i class="fas fa-chevron-down down"></i>',
+})
